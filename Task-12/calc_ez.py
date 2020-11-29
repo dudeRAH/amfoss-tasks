9cdc507f42a55cd26e22e28e056d8d06921e4953
dudeRAH <73278325+dudeRAH@users.noreply.github.com>
2020-11-29 23:24:03 +0530

    Update calc_ez.py

--- Task-12/calc_ez.py
+++ Task-12/calc_ez.py
@@ -13,16 +13,14 @@ T=input();
 if(int(T)==0):
     sys.exit("Number of test cases should be between 1 and 100000");
 for i in range(int(T)):
-    tmpstr=input()
-    res=tmpstr.split(',');
-    b=float(res[0]);
-    
-    c=float(res[1]);
-    
-    nslices=1000;
-    deltax=.00000001;
-    check=10e22;
-    tol=.01;
+    b=input()
+    b=float(b)
+    c=input()
+    c-float(c)
+    nslices=1000; #slicing the graph into smaller units to find effective slope at a point
+    deltax=.00000001; #assuming a small value for delta x and finding other attributes and slope via the assumed value
+    value=10e22; # very high value in order to swap with the temporary variable to get the smallest value
+    tol=.01; #tolerance value below which is considered as zero for slope determination
     for x in np.arange(0.0,pi/2.0,pi/(2.*nslices)):
         x1=x-deltax/2.0
         x2=x+deltax/2.0
@@ -30,10 +28,9 @@ for i in range(int(T)):
         y2=myfunc(x2,b,c);
         deltay=y2-y1;
         slope=abs(deltay/deltax);
-        #print(slope)
         if(slope<=tol):
             tmp=myfunc(x,b,c);
-            if(tmp<check):
-                check=tmp;
-    print('The minimum value is',check)
+            if(tmp<value):
+                value=tmp;
+    print('The minimum value is',value)
 
